925d105b61c5faec41c41f946912e3ce1a697aef
Edizon1 <86514881+Edizon1@users.noreply.github.com>
2021-07-05 10:40:31 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>☄Check links at @malayalammovies639 ☄ Found</i> <code>{leng}</code> <i>Results For Your Request 🕸:</i> <code>{query}</code>"
+    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Request 🕸:</i> <code>{query}</code>"
         
     try:
         await update.message.edit(
