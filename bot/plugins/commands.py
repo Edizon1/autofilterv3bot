d7e4f0107ef19dc714e5e7c818e0c62eeaa20760
Edizon1 <86514881+Edizon1@users.noreply.github.com>
2021-08-09 12:59:47 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = caption = f"{file_name} \n @malayalammoviestg1 \n @filimfiles10 \n @mallumoviesuggest \n @malayalammoviesD",
+                caption = f"{file_name} \n @malayalammoviestg1 \n @filimfiles10 \n @mallumoviesuggest \n @malayalammoviesD",
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
